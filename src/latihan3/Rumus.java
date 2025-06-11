671103516fe9a17ebb68469e3fc7a3507fc2d11b
MhmmdBayuAdullah <mhmmdbayuabdllh@gmail.com>
2025-06-11 15:50:40 +0800

    membuat mutator

--- src/latihan3/Rumus.java
+++ src/latihan3/Rumus.java
@@ -2,5 +2,20 @@ package latihan3;
 
 public class Rumus {
     //Muhammad Bayu Abdullah, NPM : 2310010488, Kelas : 4C TI REGP BJB
-    double sisi, panjang, lebar, hasil;
+    private double sisi, panjang, lebar, hasil;
+    
+    public Rumus(double sisi, double panjang, double lebar, double hasil){
+        this.sisi = sisi;
+        this.panjang= panjang;
+        this.panjang= lebar;
+        this.panjang= hasil;
+    }
+    
+    double setLuasPersegi(){
+        return sisi * sisi;
+    }
+    
+    double setLuasPersegiPanjang(){
+        return panjang * lebar;
+    }
 }
